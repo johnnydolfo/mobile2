f4d4698ef061924541b756657523cd7495abaefe
johnnydolfo <jbergerandrade@gmail.com>
2025-05-08 12:00:51 -0300

    parte do codigo

--- app/src/main/java/com/example/ex2/MainActivity.kt
+++ app/src/main/java/com/example/ex2/MainActivity.kt
@@ -5,11 +5,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.ArrayAdapter
+import com.example.ex2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val items = listOf("masculino", "feminino")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, items)
+
+        binding.button
     }
 }

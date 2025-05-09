c63b4f3f1ef84f6ced566346e235c1cfb418ce2a
PICHAU <jrbergerandrade@gmail.com>
2025-05-08 22:47:36 -0300

    commit teste

--- app/src/main/java/com/example/ex2/MainActivity.kt
+++ app/src/main/java/com/example/ex2/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.ex2
 
+import android.R.attr.text
+import android.app.Activity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.widget.ArrayAdapter
 import com.example.ex2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,9 +18,33 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val items = listOf("masculino", "feminino")
-        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, items)
+        val generos = listOf("Masculino", "Feminino")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, generos)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spinnerSexo.adapter = adapter
 
-        binding.button
+        binding.botao.setOnClickListener {
+            val idadeTexto = binding.textoidade.text.toString()
+            val generoSelecionado = binding.spinnerSexo.selectedItem.toString()
+
+            if (idadeTexto.isNotEmpty()) {
+                val idade = idadeTexto.toInt()
+                val resultado = when (generoSelecionado) {
+                    "Masculino" -> 65 - idade
+                    "Feminino" -> 62 - idade
+                    else -> 0
+                }
+
+                val mensagem = if (resultado <= 0) {
+                    "Você já pode fazer sua aposentadoria."
+                } else {
+                    "Restam $resultado anos até que você possa fazer sua aposentadoria."
+                }
+
+                binding.tvResultado.text = mensagem
+            } else {
+                binding.tvResultado.text = "Por favor, insira sua idade."
+            }
+        }
     }
 }
